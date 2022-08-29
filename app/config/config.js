fa795d6a4ea062f3b8d0e103997aab8234070ece
Segsolin <segunisrael4@gmail.com>
2022-08-29 18:51:32 +0100

    init

--- app/config/config.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('email', []);
-
-app.config(['$httpProvider', function ($httpProvider) {
-
-    $httpProvider.defaults.headers.common['Accept'] = "application/json";
-    $httpProvider.defaults.headers.common['Content-Type'] = "application/json";
-    $httpProvider.defaults.useXDomain = true;
-
-}]);
