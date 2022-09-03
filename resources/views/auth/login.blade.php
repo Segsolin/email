2b2b29171aef075a0086b850519374fe154d604e
Segsolin <segunisrael4@gmail.com>
2022-09-03 10:04:05 +0100

    logo fixed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,17 +39,25 @@
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
             </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
+            <div class="flex items-center justify-center mt-4">
+                <x-button class="ml-3">
+                    {{ __('Log in') }}
+                </x-button>
+            </div>
+                <div class="flex items-center justify-center mt-4">
+                Not Registered? Click here
+                <x-button class="ml-3">
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                    {{ __('Register') }}
+                     </a>
+                </x-button>
+            </div>
+                <div class="flex items-center justify-center mt-4">
+               @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
-
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
             </div>
         </form>
     </x-auth-card>
